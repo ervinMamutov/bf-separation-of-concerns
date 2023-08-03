6e01b2e4325c8d57ccd985f94631a5af4c223646
ervinMamutov <ervin.news@gmail.com>
2023-08-03 16:35:03 +0200

    finish exerses-write-test

--- 5-testing-components/exercises-write-tests/classy-header/classy-header.js
+++ 5-testing-components/exercises-write-tests/classy-header/classy-header.js
@@ -8,15 +8,17 @@
  * @param {string[]} [classList=[]] - an array of class names
  * @returns {HTMLHeadingElement} a rendered header element
  */
-export const classyHeader = (level, text, classList = []) => {
-    if (level < 1 || 6 < level) {
-        throw new RangeError('level must be in range: 1 -> 6');
-    }
+const classyHeader = (level, text, classList = []) => {
+  if (level < 1 || 6 < level) {
+    throw new RangeError('level must be in range: 1 -> 6');
+  }
 
-    const headerEl = document.createElement(`h${level}`);
-    headerEl.innerHTML = text;
-    for (const className of classList) {
-        headerEl.classList.add(className);
-    }
-    return headerEl;
+  const headerEl = document.createElement(`h${level}`);
+  headerEl.innerHTML = text;
+  for (const className of classList) {
+    headerEl.classList.add(className);
+  }
+  return headerEl;
 };
+
+export default classyHeader;
--- 5-testing-components/exercises-write-tests/classy-header/classy-header.spec.js
+++ 5-testing-components/exercises-write-tests/classy-header/classy-header.spec.js
@@ -1,53 +1,57 @@
-import { classyHeader } from './classy-header.js';
+/**
+ * @jest-environment jsdom
+ */
+
+import classyHeader from './classy-header.js';
 
 describe('classyHeader: renders DOM headers of different levels and classes', () => {
-    describe('an H3 with text: "hello", and no classes', () => {
-        const actual = classyHeader(3, 'hello');
-        it('has tagName "H3"', () => {
-            _;
-        });
-        it('has innerHTML: "hello"', () => {
-            _;
-        });
-        it('has classList length 0', () => {
-            _;
-        });
-        it('has className: ""', () => {
-            _;
-        });
-        it('has childElementCount: 0', () => {
-            _;
-        });
+  describe('an H3 with text: "hello", and no classes', () => {
+    const actual = classyHeader(3, 'hello');
+    it('has tagName "H3"', () => {
+      expect(actual.nodeName).toEqual('H3');
     });
-    describe('an H1 with text: "good bye", and two classes', () => {
-        const actual = classyHeader(_, _, _);
-        it('has tagName "H1"', () => {
-            _;
-        });
-        it('has innerHTML: "good bye"', () => {
-            _;
-        });
-        it('has classList length 2', () => {
-            _;
-        });
-        it('have class: "fancy"', () => {
-            _;
-        });
-        it('have class: "hover-right"', () => {
-            _;
-        });
-        it('has childElementCount: 0', () => {
-            _;
-        });
+    it('has innerHTML: "hello"', () => {
+      expect(actual.innerHTML).toEqual('hello');
     });
-    describe('does not allow invalid header levels', () => {
-        it('throws an error if level is less than 1', () => {
-            const shouldThrow = () => classyHeader(0, 'oops!');
-            expect(shouldThrow).toThrowError(RangeError);
-        });
-        it('throws an error if level is greater than 6', () => {
-            const shouldThrow = () => classyHeader(7, 'oops!');
-            expect(shouldThrow).toThrowError(RangeError);
-        });
+    it('has classList length 0', () => {
+      expect(actual.classList.length).toEqual(0);
     });
+    it('has className: ""', () => {
+      expect(actual.className).toEqual('');
+    });
+    it('has childElementCount: 0', () => {
+      expect(actual.childElementCount).toEqual(0);
+    });
+  });
+  describe('an H1 with text: "good bye", and two classes', () => {
+    const actual = classyHeader(1, 'good bye', ['fancy', 'hover-right']);
+    it('has tagName "H1"', () => {
+      expect(actual.nodeName).toEqual('H1');
+    });
+    it('has innerHTML: "good bye"', () => {
+      expect(actual.innerHTML).toEqual('good bye');
+    });
+    it('has classList length 2', () => {
+      expect(actual.classList.length).toEqual(2);
+    });
+    it('have class: "fancy"', () => {
+      expect(actual.classList.contains('fancy')).toEqual(true);
+    });
+    it('have class: "hover-right"', () => {
+      expect(actual.classList.contains('hover-right')).toEqual(true);
+    });
+    it('has childElementCount: 0', () => {
+      expect(actual.childElementCount).toEqual(0);
+    });
+  });
+  describe('does not allow invalid header levels', () => {
+    it('throws an error if level is less than 1', () => {
+      const shouldThrow = () => classyHeader(0, 'oops!');
+      expect(shouldThrow).toThrowError(RangeError);
+    });
+    it('throws an error if level is greater than 6', () => {
+      const shouldThrow = () => classyHeader(7, 'oops!');
+      expect(shouldThrow).toThrowError(RangeError);
+    });
+  });
 });
